3bc4e8d8d6bf16eb724d27045cfc7042cfeef497
Emmanuel <eferreyramontes@gmail.com>
2018-04-25 00:02:27 -0300

    Clase 3 - Utilizando JS moderno en nuesrto código

--- babel-loader/index.js
+++ babel-loader/index.js
@@ -1,6 +1,7 @@
 import './styles.css';
-import { firstMessage } from './message.js';
+import { firstMessage, delayedMessage } from './message.js';
 
 document.write(firstMessage);
+delayedMessage();
 
 console.log('Hola mundo desde un módulo');
